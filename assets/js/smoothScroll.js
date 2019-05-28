14957f2aa572072b408651af87ddcf184631258a
Al Cheong <22002891+meeps123@users.noreply.github.com>
2019-05-28 09:21:52 +0800

    Fix smooth scrolling & page width

--- assets/js/smoothScroll.js
+++ assets/js/smoothScroll.js
@@ -1,18 +1,15 @@
-$('a.nav-link').on('click', function (e) {
-	e.preventDefault();
-	let hash = this.hash;
-	let currScrollPosition = $('div.content').scrollTop();
-	let hashPosition = $(hash).offset().top-15;
-	$('div.content').animate({
-		scrollTop: currScrollPosition+hashPosition
-	}, 1000, 'swing', function () {
-		window.location.hash = hash;
+if (isOnMobile()) {
+	// you are on a mobile device
+	$('a.nav-link').on('click', function (e) {
+		e.preventDefault();
+		let hash = this.hash;
+		// let currScrollPosition = $('div.content').scrollTop();
+		let hashPosition = $(hash).offset().top-15;
+		window.scrollTo(0, hashPosition);
+		return false;
 	});
-	return false;
-});
-
-function setupSmoothScrollForCitations() {
-	$('a.citation-link').on('click', function (e) {
+} else {
+	$('a.nav-link').on('click', function (e) {
 		e.preventDefault();
 		let hash = this.hash;
 		let currScrollPosition = $('div.content').scrollTop();
@@ -24,4 +21,34 @@ function setupSmoothScrollForCitations() {
 		});
 		return false;
 	});
+}
+
+
+function setupSmoothScrollForCitations() {
+	if (isOnMobile()) {
+		$('a.citation-link').on('click', function (e) {
+			e.preventDefault();
+			let hash = this.hash;
+			let hashPosition = $(hash).offset().top-15;
+			window.scrollTo(0, hashPosition);
+			return false;
+		});
+	} else {
+		$('a.citation-link').on('click', function (e) {
+			e.preventDefault();
+			let hash = this.hash;
+			let currScrollPosition = $('div.content').scrollTop();
+			let hashPosition = $(hash).offset().top-15;
+			$('div.content').animate({
+				scrollTop: currScrollPosition+hashPosition
+			}, 1000, 'swing', function () {
+				window.location.hash = hash;
+			});
+			return false;
+		});
+	}
+}
+
+function isOnMobile() {
+	return navigator.userAgent.match(/(iPod|iPhone|iPad|Android)/);
 }
